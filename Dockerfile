96e40ab26f7f6013b5af09182caf1eef43d5467c
pangliang <418094911@qq.com>
2023-01-09 14:45:59 +0800

    add github actions

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,17 @@
 FROM ubuntu:16.04
 
-RUN sed -i "s@archive.ubuntu.com@mirrors.aliyun.com@g" /etc/apt/sources.list \
-	&& sed -i "s@security.ubuntu.com@mirrors.aliyun.com@g" /etc/apt/sources.list \
-	&& apt-get update \
-	&& apt-get install -y wget procps 
+# RUN sed -i "s@archive.ubuntu.com@mirrors.aliyun.com@g" /etc/apt/sources.list \
+#     && sed -i "s@security.ubuntu.com@mirrors.aliyun.com@g" /etc/apt/sources.list
+RUN apt-get update \
+    && apt-get install -y wget procps telnet
 
-ENV SDK_NAME=FutuOpenD_5.2.1412_Ubuntu16.04
+ARG VERSION
+ENV SDK_NAME=FutuOpenD_${VERSION}_NN_Ubuntu16.04
 
-RUN wget -O - https://softwarefile.futunn.com/${SDK_NAME}.tar.gz | tar -xzf - -C / \
-	&& rm -rf /${SDK_NAME}/${SDK_NAME}.AppImage \
-	&& ln -s /${SDK_NAME}/${SDK_NAME} /FutuOpenD
+RUN wget -O - https://softwarefile.futunn.com/${SDK_NAME}.tar.gz | tar -xzf - -C /
+RUN rm -rf /${SDK_NAME}/FutuOpenD-GUI_${VERSION}_NN_Ubuntu16.04 \
+    && ln -s /${SDK_NAME}/${SDK_NAME} /FutuOpenD
 
 WORKDIR /FutuOpenD
-
-COPY ./FutuOpenD.xml /FutuOpenD/FutuOpenD.xml
 COPY docker-entrypoint.sh /
-
-ENTRYPOINT ["/docker-entrypoint.sh"]
+CMD ["/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,12 @@ console=${console:-0}
 no_monitor=${no_monitor:-1}
 cfg_file=${cfg_file:-"$FUTU_HOME/FutuOpenD.xml"}
 lang=${lang:-chs}
+api_ip=${api_ip:-0.0.0.0}
 api_port=${api_port:-11111}
-log_level=${log_level:-no}
+telnet_ip=${telnet_ip:-127.0.0.1}
 
-OPTS="-console=$console -no_monitor=$no_monitor -cfg_file=$cfg_file -lang=$lang -api_port=$api_port -log_level=$log_level"
+OPTS="-console=$console -no_monitor=$no_monitor -cfg_file=$cfg_file -lang=$lang -log_level=$log_level"
+OPTS="$OPTS -api_ip=$api_ip -api_port=$api_port -telnet_ip=$telnet_ip -telnet_port=$telnet_port -websocket_ip=$websocket_ip -websocket_port=$websocket_port"
 
 if [ $login_account ];then
 	OPTS="$OPTS -login_account=$login_account"
@@ -17,6 +19,16 @@ else
 	echo "login_account IS NOT EXISTS"
     exit 100
 fi
+
+if [ $login_cert_file ];then
+	OPTS="$OPTS -login_cert_file=$login_cert_file"
+fi
+
+if [ $login_cert_file ];then
+	OPTS="$OPTS -login_region=$login_region"
+fi
+
+echo "Use Opts $OPTS"
 
 if [ $login_pwd ];then
 	OPTS="$OPTS -login_pwd=$login_pwd"
@@ -30,16 +42,6 @@ if [ $login_pwd_md5 -a $login_pwd ];then
     echo "login_pwd IS NOT EXISTS"
     exit 100
 fi
-
-if [ $login_cert_file ];then
-	OPTS="$OPTS -login_cert_file=$login_cert_file"
-fi
-
-if [ $login_cert_file ];then
-	OPTS="$OPTS -login_region=$login_region"
-fi
-
-echo "Use Opts $OPTS"
 
 $FUTU_HOME/FutuOpenD $OPTS
 
